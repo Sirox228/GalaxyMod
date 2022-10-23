00cbb19ea9b25e649d94baca8da464de3e67b5a1
MasterX <95196453+Default-name123@users.noreply.github.com>
2022-10-23 00:10:44 -0500

    Stable two

--- source/Options.hx
+++ source/Options.hx
@@ -409,7 +409,7 @@ class FPSOption extends Option
 	public override function right():Bool
 	{
 		if (!FlxG.save.data.FPS)
-			FlxG.save.data.FPS = 120;
+			FlxG.save.data.FPS = 60;
 		if (FlxG.save.data.FPS < 240)
 			FlxG.save.data.FPS += 1;
 		display = updateDisplay();
